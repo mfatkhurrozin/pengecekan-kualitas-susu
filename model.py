ecf745e426611168c62cdd1cc4f84a7f8b68e28c
Muhammad Fatkhurrozin <m.fatkhurrozin@gmail.com>
2022-09-29 11:00:35 +0700

    Update model.py

--- model.py
+++ model.py
@@ -13,16 +13,16 @@ import time
 # import the data
 data = pd.read_csv("milknewfix.csv")
 image = Image.open("susu.jpg")
-st.title("Selamat datang di Aplikasi Pengecekan Kualitas Susu")
+st.title("Selamat datang di Webiste Pengecekan Kualitas Susu")
 st.image(image, use_column_width=True)
 
 # checking the data
-st.write("Aplikasi ini bertujuan unutuk membantu Mengecek Kualitas Susu")
-check_data = st.checkbox("Lihat contoh Data")
+st.write("Aplikasi ini Bertujuan Untuk Membantu Mengecek Kualitas Susu")
+check_data = st.checkbox("Lihat Contoh Data")
 if check_data:
     st.write(data[1:10])
-st.write("Sekarang masukan data parameter untuk melihat hasil pengecekan")
-
+st.write("Sekarang Masukan Data Parameter Untuk Melihat Hasil Pengecekan")
+ 
 # input the numbers
 ph = st.slider("PH Pada Susu", float(
     data.ph.min()), float(data.ph.max()), float(data.ph.mean()))
@@ -30,12 +30,40 @@ temprature = st.slider("Temperature", int(
     data.temprature.min()), int(data.temprature.max()), int(data.temprature.mean()))
 # taste = st.slider("Apakah Rasa Susu Enak? (1=Ya|0=Tidak)", int(data.taste.min()), int(
 #     data.taste.max()), int(data.taste.mean()))
-odor = st.slider("Apakah Bau Susu Enak? (1=Ya|0=Tidak)", int(data.odor.min()), int(
-    data.odor.max()), int(data.odor.mean()))
-fat = st.slider("Kadar Lemak Pada Susu? (1=Tingggi|0=Rendah)", int(data.fat.min()), int(
-    data.fat.max()), int(data.fat.mean()))
-turbidity = st.slider("Kekeruhan Warna Pada Susu? (1=Tingggi|0=Rendah)", int(data.turbidity.min()), int(
-    data.turbidity.max()), int(data.turbidity.mean()))
+# odor = st.slider("Apakah Bau Susu Enak? (1=Ya|0=Tidak)", int(data.odor.min()), int(
+#     data.odor.max()), int(data.odor.mean()))
+
+odor1 = st.radio(
+    "Apakah Rasa Susu Enak?",
+    ('Ya', 'Tidak'))
+
+if odor1 == 'Ya':
+    odor = 1
+else:
+    odor = 0
+
+fat1 = st.radio(
+    "Bagaimana Tingkat Lemak?",
+    ('Tinggi', 'Rendah'))
+
+if fat1 == 'Tinggi':
+    fat = 1
+else:
+    fat = 0
+
+turbidity1 = st.radio(
+    "Bagaimana Tingkat Kekeruhan?",
+    ('Tinggi', 'Rendah'))
+
+if turbidity1 == 'Tinggi':
+    turbidity = 1
+else:
+    turbidity = 0
+
+# fat = st.slider("Kadar Lemak Pada Susu? (1=Tingggi|0=Rendah)", int(data.fat.min()), int(
+#     data.fat.max()), int(data.fat.mean()))
+# turbidity = st.slider("Kekeruhan Warna Pada Susu? (1=Tingggi|0=Rendah)", int(data.turbidity.min()), int(
+#     data.turbidity.max()), int(data.turbidity.mean()))
 colour = st.slider("Tingkat Warna pada susu", float(data.colour.min()), float(
     data.colour.max()), float(data.colour.mean()))
 
@@ -46,26 +74,27 @@ y = data['grade']
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=20, random_state=42)
 
-# # modelling step
-# # Linear Regression model
-# # import your model
+# modelling step
+# Linear Regression model
+# import your model
 # model = LinearRegression()
 # # fitting and predict your model
 # model.fit(X_train, y_train)
 # model.predict(X_test)
 # errors = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
 # predictions = model.predict(
-#     [[temp, humid, tv, odor, fat, turbidity]])[0]
+#     [[ph, temprature, odor, fat, turbidity, colour]])[0]
 # akurasi = np.sqrt(r2_score(y_test, model.predict(X_test)))
 
 # =============================================================================
-# #RandomForestModel
-#model2 = RandomForestRegressor(random_state=0)
-# model2.fit(X_train,y_train)
+# RandomForestModel
+# model2 = RandomForestRegressor(random_state=0)
+# model2.fit(X_train, y_train)
 # model2.predict(X_test)
-#errors = np.sqrt(mean_squared_error(y_test,model2.predict(X_test)))
-#predictions = model2.predict([[temp,humid,tv,odor,fat,turbidity]])[0]
-#akurasi= np.sqrt(r2_score(y_test,model2.predict(X_test)))
+# errors = np.sqrt(mean_squared_error(y_test, model2.predict(X_test)))
+# predictions = model2.predict(
+#     [[ph, temprature, odor, fat, turbidity, colour]])[0]
+# akurasi = np.sqrt(r2_score(y_test, model2.predict(X_test)))
 # =============================================================================
 
 # =============================================================================
@@ -87,8 +116,8 @@ elif predictions == 1:
 else:
     hasil = ("Kualitas Tinggi")
 
-# hasilakurasi = akurasi*100
+hasilakurasi = akurasi*100
 # checking prediction house price
 if st.button("Cek!"):
     st.header("Hasil Pengecekan : {}".format(hasil))
-    # st.subheader("Akurasi : {}".format(hasilakurasi))
+    st.subheader("Akurasi : {}".format(hasilakurasi))
